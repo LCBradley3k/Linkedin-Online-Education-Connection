b6f0d6cbfa56da1b98a05587fdc43576aca0d60c
Mac User <BradleyMitchellis@gmail.com>
2019-02-04 21:50:25 -0500

    install and setup initial firebase

--- online-footprint-app/src/app/app.module.ts
+++ online-footprint-app/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(
+        {
+            apiKey: environment.FIREBASE_API_KEY,
+            authDomain: "online-footprint.firebaseapp.com",
+            databaseURL: "https://online-footprint.firebaseio.com",
+            projectId: "online-footprint",
+            storageBucket: "online-footprint.appspot.com",
+            messagingSenderId: "844518887878"
+          }
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
