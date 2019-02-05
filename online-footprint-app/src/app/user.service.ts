9fe89db7226e456eca6da294c265ea4f7bbd63fb
Mac User <BradleyMitchellis@gmail.com>
2019-02-05 11:43:08 -0500

    Add blank user service

---
+++ online-footprint-app/src/app/user.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { of as observableOf } from 'rxjs'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+}
